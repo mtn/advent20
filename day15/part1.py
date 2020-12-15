bc7d9bd45443c5c9f1bc6bca831929cd9d8ae6d6
Michael Noronha <michaeltnoronha@gmail.com>
2020-12-14 23:52:13 -0600

    day 15

---
+++ day15/part1.py
@@ -0,0 +1,20 @@
+from itertools import count
+
+inp = "15,12,0,14,3,1"
+nums = list(map(int, inp.split(",")))
+
+ls = {n:i for i,n in enumerate(nums)}
+
+spoken = len(nums) - 1
+while len(nums) < 2020:
+    cur = nums[-1]
+    if cur not in ls:
+        ls[cur] = len(nums) - 1
+        nums.append(0)
+    else:
+        nums.append(len(nums) - 1 - ls[cur])
+        ls[cur] = len(nums) - 2
+
+print(nums[-1])
+
+
